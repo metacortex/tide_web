f82f828ab886e30b0570d2d96576d490b5a97af1
Jung Wook Son <9ruvie@gmail.com>
2011-09-25 20:18:00 +0900

    main#index design

--- web/app/assets/javascripts/coffeescripts/invitation.js
+++ web/app/assets/javascripts/coffeescripts/invitation.js
@@ -6,4 +6,7 @@ if (f.length > 0) {
   moreFriendForm();  
 }
 
+// Under Construction!
+$("a.comming_soon, .comming_soon a").bind("click", function(){ alert("Under Construction ^^;");return false;});
+
 });
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -83,7 +83,8 @@ Web::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => redirect("/posts")
+  # root :to => redirect("/posts")
+  root :to => "main#index"
 
   # See how all your routes lay out with "rake routes"
 
