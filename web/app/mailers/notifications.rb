8afce5752a916abe96c8fa18920591a3614eb093
Jung Wook Son <9ruvie@gmail.com>
2012-02-29 01:06:18 +0900

    password_reset mail

--- web/app/mailers/notifications.rb
+++ web/app/mailers/notifications.rb
@@ -1,14 +1,14 @@
 class Notifications < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@tideinstitute.org"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notifications.reset_password.subject
   #
-  def reset_password
+  def reset_password(user)
     @user = user
-    @url  = "http://0.0.0.0:3000/password_resets/#{user.reset_password_token}/edit"
+    @url  = "http://www.tideinstitute.org/password_resets/#{user.reset_password_token}/edit"
     mail(:to => user.email,
          :subject => "Your password has been reset")
   end
--- web/config/initializers/setup_mail.rb
+++ web/config/initializers/setup_mail.rb
@@ -1,19 +1,19 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
+  :address              => "sender.oxygencomputing.com",
+  :port                 => 25,
   :domain               => "tideinstitute.org",
-  :user_name            => "tideinstitute",
-  :password             => "tideinst",
+  :user_name            => "postman@sender.oxygencomputing.com",
+  :password             => "!l0v3spams",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
 
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.subject = "#{message.to} #{message.subject}"
-    message.to = "tideinstitute@tideinstitute.org"
-  end
-end
-
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
-Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+# class DevelopmentMailInterceptor
+#   def self.delivering_email(message)
+#     message.subject = "#{message.to} #{message.subject}"
+#     message.to = "no-reply@tideinstitute.org"
+#   end
+# end
+# 
+ActionMailer::Base.default_url_options[:host] = "tideinstitute.org"
+# Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
